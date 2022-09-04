4c6a9500b6588f1aaf90502903c0f8acc4f4c846
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-04 02:03:08 -0500

    Create metodos.py

---
+++ metodos.py
@@ -0,0 +1,13 @@
+p="enrique barros fernández".title()
+
+print(p)
+
+p="Esta Es Una Frase Para Ser Formateada."
+c=p.upper()
+
+print(c)
+
+
+E="Esta Es Una Frase Para Ser Formateada."
+
+print(E.lower())

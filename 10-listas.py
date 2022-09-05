0b2cca5df268baa317df6c642c4ddc2e89e13857
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 13:52:27 -0500

    Create 10-listas.py
    hacer listas con orden

---
+++ 10-listas.py
@@ -0,0 +1,9 @@
+colores = ["rojo", "azul", "verde", "amarillo", "marrón", "lila", "negro", "rosa"]
+
+print("roja 0 y rosa 7")
+print(colores[3])
+
+h=" perros "
+numeros = ["tes", "dos", "cinco", "cuatro", "uno"]
+
+print(numeros[3]+h)

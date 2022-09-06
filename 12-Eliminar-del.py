d1d266ede6425e2e36a5eedaba9ae094873f948e
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 22:22:02 -0500

    Create 12-Eliminar-del.py

---
+++ 12-Eliminar-del.py
@@ -0,0 +1,6 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+del colores[1] #nOMBRE DE LA LSITA Y POSICION N O P
+del colores[4]
+del colores[-4]
+del colores[-3]
+print(colores)

1d80295569f9285ffb416c042dfdb8ee247166db
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 22:22:08 -0500

    Create 14-Eliminar-elementos-pop.py

---
+++ 14-Eliminar-elementos-pop.py
@@ -0,0 +1,8 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+color1 = colores.pop(1)
+color2 = colores.pop(7)
+
+colores_guardados = [color1, color2]
+print(colores_guardados)
+print(colores)

0082521ddbbb3da749e1f90e126a148bf1ebb016
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 23:45:53 -0500

    Create 20-tuplas-listas.py

---
+++ 20-tuplas-listas.py
@@ -0,0 +1,9 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja']#lista
+
+tupla = tuple(colores)
+ #lista = list(tupla)
+
+
+print(type(colores))
+print(type(tupla))
+print (tupla)

a6cb0779423e34a3c0b9a8e91816fed15d940c77
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 23:52:04 -0500

    Create 21-IF-operadores-comparación.py

---
+++ 21-IF-operadores-comparación.py
@@ -0,0 +1,19 @@
+num1 = 15
+num2 = 20
+
+if num1 != num2:
+	print('Se cumplio el el if.')
+
+
+num1 = 1450
+num2 = 60
+
+if num1 > num2:
+	print('Se ejecuta el if.')
+
+
+num1 = 60
+num2 = 60
+
+if num1 != num2:
+	print('Se ejecuta el if.')

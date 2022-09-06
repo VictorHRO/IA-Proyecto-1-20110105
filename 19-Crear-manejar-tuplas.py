701463f288dd7d2089b7a09828b6f6ee0562ae42
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 23:35:17 -0500

    Create 19-Crear-manejar-tuplas.py
    las tuplas no pueden, son como listas constantes que no se pueden modificar
    
    inmutables

---
+++ 19-Crear-manejar-tuplas.py
@@ -0,0 +1,9 @@
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marrón', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+
+print(colores[1])
+
+numeros = (10, 1, 5, 11) # listas pero no se pueden modificar de ninguna manera 
+
+operacion = numeros[0] + numeros[2] + numeros[3] - numeros[1]
+
+print(operacion)

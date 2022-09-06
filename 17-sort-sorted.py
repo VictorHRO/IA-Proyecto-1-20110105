9350a57271ecb26a12e7ff2980ef9a523d29866a
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-05 23:11:18 -0500

    Create 17-sort-sorted.py
    A-z     z-a

---
+++ 17-sort-sorted.py
@@ -0,0 +1,8 @@
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marrón',
+          'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+colores.sort(reverse=True)#permanente
+
+print(colores)
+
+print(sorted(colores))#solo a esta linea 

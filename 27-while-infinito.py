70308de1b263dd76b7bf8ff8ee1755ffb6efcd14
VictorHRO <112038677+VictorHRO@users.noreply.github.com>
2022-09-15 17:25:33 -0500

    Create 27-while-infinito.py

---
+++ 27-while-infinito.py
@@ -0,0 +1,19 @@
+x = 0
+
+while x <= 15:
+	print("bucle 1 ", x)
+	x += 5
+
+x=0
+while x >= -100:
+	print("bucle 2 ", x)
+	x -= 20
+
+	
+
+y = 10
+
+while y >= 0:
+ 	 
+ print(" El valor del bucle es  ", y)    
+ y -= 1
